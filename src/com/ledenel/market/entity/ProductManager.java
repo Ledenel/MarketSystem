60bac1e4f365e4c085e662fa2e485cd78a224c73
Ledenel <ledenelintelli@gmail.com>
2016-03-25 00:38:24 +0900

    add new methods.

--- src/com/ledenel/market/entity/ProductManager.java
+++ src/com/ledenel/market/entity/ProductManager.java
@@ -1,6 +1,7 @@
 package com.ledenel.market.entity;
 
 import java.util.Collection;
+import java.util.Date;
 
 /**
  * Created by Ledenel on 2016/3/18.
@@ -11,15 +12,25 @@ public interface ProductManager extends ProductScannerListener, ProductListener 
     void sellProduct(Product product);
 
     Product getProduct(long uid);
+
     Collection<Product> getProducts(String name);
 
+    Collection<Product> getExpiredProducts(Date expiredDate);
+
     void attachProductSellScanner(ProductScanner scanner);
+
     void detachProductSellScanner(ProductScanner scanner);
+
     void attachProductStockScanner(ProductScanner scanner);
+
     void detachProductStockScanner(ProductScanner scanner);
+
     void attachProductEventListener(ProductEventListener listener);
+
     void detachProductEventListener(ProductEventListener listener);
+
     void attachFundEventListener(FundEventListener listener);
+
     void detachFundEventListener(FundEventListener listener);
 
 }
